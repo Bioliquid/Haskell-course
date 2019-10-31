ad39cb5db4adfbc56d1c8f676f7362a49f5ab7d3
Larionov Nikita <iksiygrik@gmail.com>
2019-10-31 13:36:49 +0300

    simple impl of 1st task

--- hw1/main.hs
+++ hw1/main.hs
@@ -4,6 +4,14 @@ module Main where
 bell :: Int -> Integer
 bell = head . (iterate (last >>= scanl (+)) [1] !!) . pred
 
+-- simple implementation of 1st task
+choose n k = product [x | x <- [k+1..n]] / product [x | x <- [1..n-k]]
+
+bell'' _ 0 = 1
+bell'' n k = (choose n k) * (bell' k) + bell'' n (k - 1)
+
+bell' n = bell'' (n - 1) (n - 1)
+
 -- 2nd task
 transpose :: [[a]] -> [[a]]
 transpose [] = []
